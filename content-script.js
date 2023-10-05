1a441187bd4a27e6c7998dbb03ae6419ae4e41da
Navin <navin09@users.noreply.github.com>
2023-10-06 02:59:03 +0530

    Added check for links on body readyState complete as well

--- content-script.js
+++ content-script.js
@@ -1,3 +1,23 @@
+function fixRedirects(node) {
+  let theLink = node.href;
+  let query = new URLSearchParams(theLink);
+  let redirUrl = query.get("q");
+  node.href = redirUrl;
+  let redirQuery = new URL(redirUrl);
+  node.title = redirQuery.hostname;
+}
+
+document.onreadystatechange = function () {
+  if (document.readyState === "complete") {
+    var nodes = document.querySelectorAll(
+      "a[href^='https://www.youtube.com/redirect'"
+    );
+    for (var node in nodes) {
+      fixRedirects(node);
+    }
+  }
+};
+
 let observer = new MutationObserver(function (mutations) {
   for (let mutation of mutations) {
     for (let addedNode of mutation.addedNodes) {
@@ -6,12 +26,7 @@ let observer = new MutationObserver(function (mutations) {
         addedNode.hasAttribute("href") &&
         addedNode.href.match(new RegExp("^https://www.youtube.com/redirect"))
       ) {
-        let theLink = addedNode.href;
-        let query = new URLSearchParams(theLink);
-        let redirUrl = query.get("q");
-        addedNode.href = redirUrl;
-        let redirQuery = new URL(redirUrl);
-        addedNode.title = redirQuery.hostname;
+        fixRedirects(addedNode);
       }
     }
   }
